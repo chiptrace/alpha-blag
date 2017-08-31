8d8cf8205cfa398466a88794720b374bd7cd7124
Wouter Avet <wouter.avet@telenet.be>
2017-08-31 08:54:42 +0000

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-
-
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
   
+  # valideren van email dmv logische codes : info + testen op => www.rubular.com ... https://regex101.com
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  valitades :email, presence: true,
+            length: { maximum: 100 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+
 end

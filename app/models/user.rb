c63d0c4c0217460e71e5602aa0b4be9d34f888e2
Wouter Avet <wouter.avet@telenet.be>
2017-08-31 09:44:29 +0000

    Fixd DB issue + added new database recoreds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase }
+  
   validates :username, presence: true,
-            uniqueness: { case_sensitive: false },
+            uniqueness: { case_sensitive: false },  # case_sensitive = controleren op unieke namen.
             length: { minimum: 3, maximum: 25 }
   
   # valideren van email dmv logische codes : info + testen op => www.rubular.com ... https://regex101.com
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  valitades :email, presence: true,
-            length: { maximum: 100 },
+  validates :email, presence: true, length: { maximum: 100 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 

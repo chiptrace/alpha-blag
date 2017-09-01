2e3e5d72344b056c82bf361cebdb3d128c887e8a
Wouter Avet <wouter.avet@telenet.be>
2017-09-01 13:47:55 +0000

    Added article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }         # maakt dat de velden niet leeg mogen zijn, én min 3, max 50
   validates :description, presence: true, length: { minimum: 10, maximum: 500 } # maakt dat de velden niet leeg mogen zijn, én min 10, max 500
   validates :user_id, presence: true
